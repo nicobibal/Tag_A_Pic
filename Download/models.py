326750bcca98251bd788cbad531017a1b6cced27
nicobibal <nico.bibal@gmail.com>
2021-03-12 11:43:49 +0100

    Enregistrement photo

--- Download/models.py
+++ Download/models.py
@@ -8,6 +8,7 @@ class Image(models.Model):
 
     def downloadImageFromUsername( username):
         L = instaloader.Instaloader()
+        i = 0
         L.dirname_pattern = "ImagesInsta"
         L.download_comments = False
         L.save_metadata = False
@@ -15,6 +16,8 @@ class Image(models.Model):
         L.download_videos = False
         profile = instaloader.Profile.from_username(L.context, username)
         for post in profile.get_posts():
+            L.filename_pattern = post.owner_username + "_" + str(i)
             L.download_post(post, target=profile.username)
-            image = Image( chemin= "ImagesInsta/" + post.date_utc)
+            image = Image(  chemin= "ImageInsta/" + post.owner_username + "_" + str(i) + ".jpg" )
             image.save()
+            i += 1
