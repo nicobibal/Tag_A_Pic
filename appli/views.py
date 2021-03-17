fae093013cdc33434ebf9ff00a2cbb98748282df
AdrienBlay <Adrien.BLAY@etu.uca.fr>
2021-03-17 09:58:34 +0100

    Creation de dossier

--- appli/views.py
+++ appli/views.py
@@ -20,40 +20,40 @@ def afficher(request):
     return render(request, 'Download/images.html', {'images': images} )
 
 def accueil(request):
-    return render(request, 'GestionDossier/accueil_vue.html')
+    return render(request, 'Accueil/accueil_vue.html')
 
 def connexion(request):
-    return render(request, 'GestionDossier/connexion_vue.html')
+    return render(request, 'Connexion/connexion_vue.html')
 
 def ajouterUser(request):
-    return render(request, 'GestionDossier/ajouter_utilisateurs_vue.html')
+    return render(request, 'GestionUtilisateur/ajouter_utilisateurs_vue.html')
 
 def choixParam(request):
-    return render(request, 'GestionDossier/choix_parametres_vue.html')
+    return render(request, 'Param/choix_parametres_vue.html')
 
 def erreur(request):
-    return render(request, 'GestionDossier/erreur.html')
+    return render(request, 'Erreur/erreur.html')
 
 def gererUser(request):
-    return render(request, 'GestionDossier/gerer_utilisateurs_vue.html')
+    return render(request, 'GestionUtilisateur/gerer_utilisateurs_vue.html')
 
 def importer(request):
-    return render(request, 'GestionDossier/importer_vue.html')
+    return render(request, 'Importer/importer_vue.html')
 
 def modifUser(request):
-    return render(request, 'GestionDossier/modifier_utilisateurs_vue.html')
+    return render(request, 'GestionUtilisateur/modifier_utilisateurs_vue.html')
 
 def nouveauProjet(request):
     return render(request, 'GestionDossier/nouveau_projet_vue.html')
 
 def nouvelUser(request):
-    return render(request, 'GestionDossier/nouvel_utilisateur/vue.html')
+    return render(request, 'GestionUtilisateur/nouvel_utilisateur_vue.html')
 
 def supprimerUser(request):
-    return render(request, 'GestionDossier/supprimer_utilisateurs_vue.html')
+    return render(request, 'GestionUtilisateur/supprimer_utilisateurs_vue.html')
 
 def tag(request):
-    return render(request, 'GestionDossier/tagger_vue.html')
+    return render(request, 'Tag/tagger_vue.html')
 
 
 
